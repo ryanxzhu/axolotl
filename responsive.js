d599426e69fe7463ab3390f17c642196e5e8787c
invenioscraper <invenioscraper@gmail.com>
2023-03-08 15:05:43 +1300

    moved all things related to responsiveness to its own JS file. Also trying to get mobile to show full screen

---
+++ responsive.js
@@ -0,0 +1,31 @@
+document.body.requestFullscreen();
+const container = document.querySelector('#container');
+const canvasContainer = document.querySelector('#canvas-container');
+const info = document.querySelector('#info');
+function positionContainers() {
+    if (window.innerHeight > window.innerWidth) {
+        positionContainersVertically();
+        return;
+    }
+    positionContainersHorizontally();
+}
+
+function positionContainersVertically() {
+    container.style.flexDirection = 'column';
+    canvasContainer.style.width = '100vw';
+    canvasContainer.style.height = '80vh';
+    info.style.width = '100vw';
+    info.style.height = '20vh';
+}
+
+function positionContainersHorizontally() {
+    container.style.flexDirection = 'row';
+    canvasContainer.style.width = '80vw';
+    canvasContainer.style.height = '100vh';
+    info.style.width = '20vw';
+    info.style.height = '100vh';
+}
+
+positionContainers();
+window.addEventListener('resize', positionContainers);
+window.addEventListener('orientationchange', positionContainers);
